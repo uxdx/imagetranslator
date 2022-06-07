c6f330dd9438dc1db19da9895b0ebd0131f8aa5f
uxdx <uxdx@naver.com>
2022-06-07 17:29:13 +0900

    view translate process

--- image_functions.py
+++ image_functions.py
@@ -1,54 +1,15 @@
-from cgitb import small
+import cv2
+import pytesseract
+
 from math import comb
 from typing import List, Tuple
 from PIL import Image
-def make_better_image(path):
-    """
-    API의 최대 입력 허용 사이즈인 1960 X 1960 에 맞게
-    이미지를 가공
-    ex) 700 x 14000 => ((700*2) X 1960) x4 
-    ex2) 
-    """
-    im = Image.open(path)
-    (sw,sh) = im.size # source width & height
-    print("sw, sh = "+str((sw,sh)))
-    # 최적화수치; 이걸로 빈공간에 이미지를 더 우겨넣을 수 있음.
-    optimize_row = 1960//sw # 가로방향 최대 최적화수치(정수) 0, 1, 2, ...
-    optimize_col = 1960//sh
-    # 초과수치; 이만큼 추가로 api요청이 필요함
-    oversize_row = sw//1960
-    oversize_col = sh//1960
-    print('oversize_row : '+str(oversize_row))
-    print('oversize_col : '+str(oversize_col))
-    # 초과수치에 근거한 이미지 분할 
-    rows = []
-    results = []
-    if oversize_row > 0:
-        for i in range(oversize_row):
-            # (left, upper, right, lower)
-            box = (1960 * i, 0, 1960 * (i+1), sh)
-            rows.append(im.crop(box))
-    else:
-        rows.append(im)
-    if oversize_col > 0:
-        for img in rows:
-            for i in range(oversize_col+1):
-                # (left, upper, right, lower)
-                upper = 1960 * i
-                lower = (1960 * i + sh%1960) if i is oversize_col else 1960 * (i+1)
-                print(lower)
-                box = (0, upper, sw, lower)
-                results.append(img.crop(box))
-    else:
-        import copy
-        results = copy.deepcopy(rows)
-    
-    return results
-def test_cutted_imgs(source, imgs:list):
-    (sw,sh) = source.size
-    
-    for img in imgs:
-        print(img.size)
+
+def is_image_has_text(path):
+    img = cv2.imread(path)   
+    config = ('-l kor --oem 3 --psm 11')
+    text = pytesseract.image_to_string(image=img, config=config)
+    return text
 
 def devide_img_as_height(limit:int, large_img:Image.Image) -> List[Image.Image]:
     (w,h) = large_img.size
@@ -78,12 +39,15 @@ def combine_img(small_imgs:List[Image.Image]) -> Image.Image:
     return large_img
 
 if __name__ == '__main__':
-    large_img = Image.open('large_img.jpg')
-    print(large_img.size)
-    small_imgs = devide_img_as_height(limit=1960, large_img=large_img)
-    for img in small_imgs:
-        print(img.size)
+    # large_img = Image.open('large_img.jpg')
+    # print(large_img.size)
+    # small_imgs = devide_img_as_height(limit=1960, large_img=large_img)
+    # for img in small_imgs:
+    #     print(img.size)
     
-    combined_img = combine_img(small_imgs=small_imgs)
-    combined_img.save('combined_img.jpeg', 'jpeg')
-    print(combined_img.size)
+    # combined_img = combine_img(small_imgs=small_imgs)
+    # combined_img.save('combined_img.jpeg', 'jpeg')
+    # print(combined_img.size)
+    # 파일에 한글 텍스트가 있는 지
+    text = is_image_has_text('source/non_text_image.jpg')
+    print(text)
--- translator.py
+++ translator.py
@@ -1,3 +1,4 @@
+from cgitb import small
 import requests
 from requests_toolbelt import MultipartEncoder
 import uuid
@@ -6,6 +7,33 @@ import base64
 import os
 from PIL import Image
 from functions import read_json_file
+from image_functions import combine_img, devide_img_as_height
+
+def large_image_translate(source:str,output:str):
+  # 소스를 PIL형으로 변환
+  large_source = Image.open(source)
+  # 작은 이미지로 나눔
+  print("Slicing image...")
+  small_imgs = devide_img_as_height(limit=1960, large_img=large_source)
+  # 작은 이미지들을 번역
+  print("Translating images...")
+  
+  for i in range(len(small_imgs)):
+    print(i+1, '/',len(small_imgs))
+    translated_small_img = translate_PILimg(small_imgs[i])
+    small_imgs[i] = translated_small_img
+    translated_small_img.save(f'temp/translated{i}.png', 'png')
+    
+  # 작은 이미지들을 합침
+  print("Combine images...")
+  large_output = combine_img(small_imgs=small_imgs)
+  # PIL형 큰 이미지를 save
+  print("Save at ", output)
+  if output.split('.')[-1] == 'jpg':
+      output = output.replace('jpg', 'jpeg')
+
+  large_output.save(output, output.split('.')[-1])
+
 
 
 def translate_PILimg(img:Image.Image) -> Image.Image:
@@ -30,9 +58,9 @@ def translate_PILimg(img:Image.Image) -> Image.Image:
   # 파일을 이미지로 변환
   translated_img = Image.open(translated_file_path)
   # 파일 삭제
-  print('Remove temp files...')
-  os.remove(temp_file_path)
-  os.remove(translated_file_path)
+  # print('Remove temp files...')
+  # os.remove(temp_file_path)
+  # os.remove(translated_file_path)
   # 반환
   return translated_img
 
@@ -53,28 +81,30 @@ def translate(source:str,output:str,source_lang:str,target_lang:str):
     url = " https://naveropenapi.apigw.ntruss.com/image-to-image/v1/translate"
     res = requests.post(url, headers=headers, data=m.to_string())
     # Deal with Response 
-    if res.status_code != 200:
+    if res.status_code == 200:
+        resObj = json.loads(res.text)
+        imageStr = resObj.get("data").get("renderedImage")
+        imgdata = base64.b64decode(imageStr)
+        # Save as image file
+        filename = output
+        with open(filename, 'wb') as f:
+            f.write(imgdata)
+        print('Translate succeed.')
+
+    elif res.status_code == 500:
+      # 이미지에 글자가 발견되지 않은 경우
+        import shutil
+        shutil.copy(source, output)
+        print(res.status_code, 'Error.')
+        print(source,' is coped to ', output)
+    else:
         print('Status code Error.')
         print(res.status_code)
         print(res.text)
-  
-    resObj = json.loads(res.text)
-    imageStr = resObj.get("data").get("renderedImage")
-    imgdata = base64.b64decode(imageStr)
-    # Save as image file
-    filename = output
-    with open(filename, 'wb') as f:
-        f.write(imgdata)
-    print('Translate succeed.')
 
 
 
 
 
 if __name__ == '__main__':
-    # 이미지 1920x1920 이내로 자르기
-    # source폴더의 모든 이미지파일이름 추출(to list)
-    source_img = Image.open('source/crownford1.jpg')
-    translated_img = translate_PILimg(source_img)
-
-    pass
+    large_image_translate('source/텀블러 상세3.jpg', 'output/텀블러 상세3_translated.jpg')
